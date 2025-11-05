86775d34cfeb81fa45f4aaca86526782cf134d69
Tatiana Didenko <tatiana.a.didenko@gmail.com>
2025-11-05 09:23:36 -0800

    Add author table migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,24 @@
 from flask import Flask
-from .db import db
-from flask_migrate import Migrate  
+from .db import db, migrate
+from .models import book, author
 from .routes.book_routes import bp as books_bp
 from .routes.author_routes import bp as authors_bp
-migrate = Migrate()
-from .models.book import Book
-from .models.author import Author 
+import os
+from dotenv import load_dotenv
 
 def create_app(config=None):
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dev.db"
+    load_dotenv()
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
     if config:
         app.config.update(config)
 
     db.init_app(app)
-    migrate.init_app(app, db)  
+    migrate.init_app(app, db)
 
     app.register_blueprint(books_bp)
     app.register_blueprint(authors_bp)
